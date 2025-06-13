0e526392c5f376f2999247866625412b86327d1a
GAUSS-TPAC <alantchapda@gmail.com>
2025-06-13 14:40:51 +0100

    first commit

---
+++ assurance-Medicale/src/main/java/com/Gestion/assurance/assurance_Medicale/AssuranceMedicaleApplication.java
@@ -0,0 +1,13 @@
+package com.Gestion.assurance.assurance_Medicale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssuranceMedicaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssuranceMedicaleApplication.class, args);
+	}
+
+}
---
+++ assurance-Medicale/src/test/java/com/Gestion/assurance/assurance_Medicale/AssuranceMedicaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gestion.assurance.assurance_Medicale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssuranceMedicaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
